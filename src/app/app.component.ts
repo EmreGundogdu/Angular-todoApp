babf810fce3fc6e6f73e10752072bac0fc5a5c9a
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-02-20 21:31:28 +0300

    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-todoApp';
+  user = 'Emre Gündoğdu';
+  items = [
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Sinemaya Gitmek', action: 'No' },
+    { description: 'Kodlama Yapmak', action: 'No' },
+    { description: 'Yüzmek', action: 'No' }
+  ];
 }
